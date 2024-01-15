7c37aeaa783f07d16b125d5a95fc240a3bd00a84
nuhptr <nugrahaadi733@gmail.com>
2024-01-15 15:42:24 +0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Dependencies
+
+-  [TailwindCSS](https://tailwindcss.com/docs/guides/vite)
+   > pnpm add -D tailwindcss postcss autoprefixer
+   > npx tailwindcss init -p
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+   theme: {
+      extend: {
+         colors: {
+            background: "#F1F4F5",
+         },
+      },
+   },
+   plugins: [],
+}
